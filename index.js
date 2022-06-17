7342b004f9311ec032284abde867bc570e3aeb3d
ludozing <luke9210@inno-datasys.com>
2022-06-17 14:45:10 +0900

    522

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.get('/', (req, res)=>{
     try {
         return res.status(200).json({hi: '안녕'})
-    } catch(e) {return res.status(500).json({error: e})}
+    } catch(e) {return res.status(522).json({no: e})}
 });
 
 app.listen(port, ()=>console.log(`서버 포트 ${port}`))
