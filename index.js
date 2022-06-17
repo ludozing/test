2997fb171b4f7a341b04652ca8f4bbf4229f21a0
ludozing <luke9210@inno-datasys.com>
2022-06-17 11:49:54 +0900

    첫 커밋

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.get('/', (req, res)=>{
+    try {
+        return res.status(200).json({hi: '안녕'})
+    } catch(e) {return res.status(500).json({error: e})}
+});
+
+app.listen(port, ()=>console.log(`서버 포트 ${port}`))
