efc465d1c3aa9f01b4c1e71fcf51656b6ffd8e3d
ludozing <luke9210@inno-datasys.com>
2022-06-17 15:21:35 +0900

    right

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const { User } = require('./User');
 
 app.use(cors());
